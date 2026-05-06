fc2c9384b2120d42ab70382969129b340a3dc16b
unknown <ehijearthur@gmail.com>
2026-05-06 13:32:50 +0100

    fix

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Staff = require('../model/Staff');
+const Staff = require('../model/staffs');
 const bcrypt = require('bcryptjs');
 
 
